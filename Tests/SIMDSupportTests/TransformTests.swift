08b75de7901f87f018e1fba12094df9390195a5d
Jonathan Wight <jwight@mac.com>
2023-09-09 09:21:09 -0700

    Implement altHashable to get around SIMD hashable problem. Implement codable using all scalars to get around codable problem.

--- Tests/SIMDSupportTests/TransformTests.swift
+++ Tests/SIMDSupportTests/TransformTests.swift
@@ -10,4 +10,18 @@ class TransformTests: XCTestCase {
         XCTAssertEqual("\(Transform(rotation: .init(real: 1, imag: [2, 3, 4])))", "Transform(rotation: 1, [2, 3, 4])")
         XCTAssertEqual("\(Transform(translation: [1, 2, 3]))", "Transform(translation: [1, 2, 3])")
     }
+
+    func testCodable() {
+        let transforms = [
+            Transform(),
+            Transform(scale: [3, 2, 1]),
+            Transform(rotation: .init(real: 1, imag: [2, 3, 4])),
+            Transform(translation: [1, 2, 3]),
+        ]
+        for transform in transforms {
+            let data = try! JSONEncoder().encode(transform)
+            let decoded = try! JSONDecoder().decode(Transform.self, from: data)
+            XCTAssertEqual(transform, decoded)
+        }
+    }
 }
